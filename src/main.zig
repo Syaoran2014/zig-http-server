5ae1759eb4f70556b37a1c8c0560752b13ed97d0
Billy David <billydavid14@gmail.com>
2024-06-14 17:26:08 -0500

    Convert to Http server

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,35 @@
 const std = @import("std");
-// Uncomment this block to pass the first stage
+const http = std.http;
 const net = std.net;
+
+const server_addr = "127.0.0.1";
+const server_port = 4221;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
 
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    try stdout.print("Logs from your program will appear here!\n", .{});
-
-    // Uncomment this block to pass the first stage
-    const address = try net.Address.resolveIp("127.0.0.1", 4221);
+    var address = try net.Address.resolveIp(server_addr, server_port);
+    // Address Var is a net.Address Object
+    // Server Var is a net.Server Object
     var listener = try address.listen(.{
         .reuse_address = true,
     });
     defer listener.deinit();
 
-    const connection = try listener.accept();
-    _ = try connection.stream.write("HTTP/1.1 200 OK\r\n\r\n");
-    try stdout.print("client connected!", .{});
+    const server = try listener.accept();
+    defer server.stream.close();
+
+    var client_head_buffer: [1024]u8 = undefined;
+    var http_server = http.Server.init(server, &client_head_buffer);
+
+    while (http_server.state == .ready) {
+        var request = http_server.receiveHead() catch |err| switch (err) {
+            error.HttpConnectionClosing => continue,
+            else => |e| return e,
+        };
+
+        _ = try request.respond("", .{});
+
+        try stdout.print("client connected!", .{});
+    }
 }
